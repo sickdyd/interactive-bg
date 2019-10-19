918ce315e074476160ef6d80e34685c7e8a6cf47
sickdyd <roberto.reale.ja@gmail.com>
2019-10-19 12:27:38 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import uuidv4 from 'uuidv4';
 import './App.css';
 
 function App() {
+
+  const [hive, setHive] = React.useState(createHiveBackground());
+
+  function createHiveBackground() {
+    const h = [];
+    for (let n = 0; n < 100; n++) {
+      const hex = {
+        id: uuidv4(),
+        width: '10vw',
+        height: '10vh',
+      }
+      h.push(hex);
+    }
+    return h;
+  }
+
+  function startAnimation(e) {
+
+    const i = e.target.id;
+    e.preventDefault();
+
+    const tempHexes = hive.map((e)=>{
+      if (e.id === i) {
+        e.classes = 'hex-zoom';
+        setTimeout(()=>{
+          const hexesWithoutClass = hive.map((h)=>{
+            if (h === e) {
+              h.classes = '';
+              return h;
+            } else {
+              return h;
+            }
+          })
+          setHive(hexesWithoutClass);
+        }, 3000);
+        return e;
+      } else {
+        return e;
+      }
+    })
+
+    setHive(tempHexes); 
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div className='hive-container'>
+        <div className='hive-container-bg' />
+        {hive.map( (hex) => <div
+                                  key={hex.id}
+                                  id={hex.id}
+                                  className={`hex ${hex.classes}`}
+                                  style={{ width: hex.width, height: hex.height }}
+                                  onMouseOver={(e)=>startAnimation(e)}
+                                  onTouchStart={(e)=>startAnimation(e)}
+                                />
+        )}
+        </div>
       </header>
     </div>
   );
